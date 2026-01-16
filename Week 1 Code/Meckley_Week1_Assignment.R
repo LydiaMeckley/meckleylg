7a5564fa874a2d3f822b0947ec16478d2519ec79
LydiaMeckley <meckleyl@susqu.edu>
2026-01-15 20:02:51 -0500

    trying again...
    trying again...

--- Week 1 Code/Meckley_Week1_Assignment.R
+++ Week 1 Code/Meckley_Week1_Assignment.R
@@ -34,7 +34,7 @@ e <- c(1,2.1,3.5,4,5,6,7.8,8,9.28,10,11.65,12.98,13,14,15.66)
 # Bind the vectors into a single data frame, rename the columns, and make the character vector with unique values the row names.[3 points]
 data <- cbind(a,b,c,d,e)
 data
-df <-as.data.frame(data)
+df <- as.data.frame(data)
 df
 colnames(df) <- c("Fur Color", "Type", "Age", "Height", "Weight")
 df
@@ -44,12 +44,14 @@ df
 # Remove the character vector with unique values from the data frame.[2 points] (did I do this right?)
 df[,-1]
 
-# Add 1 row with unique numeric values to the data frame.[2 points] (ask if I can add a letter to make it even)
-p <- data.frame("c",16,11,17.2)
+# Add 1 row with unique numeric values to the data frame.[2 points]
+p <- data.frame('p', 'c', 16,11,17.2)
 p
-colnames(p) <- colnames(df.a)
-df.r <-rbind(df.a, p)
+colnames(p) <- colnames(df)
+df.r <- rbind(df, p)
 p
+row.names(df.r) <- c(row.names(df[1:15]), 'p')
+df.r
 
 # Export the data frame as a .csv file [2 points]
 
