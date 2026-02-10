60a92dd4aa98a9d526146d1bb46b844584559415
LydiaMeckley <meckleyl@susqu.edu>
2026-02-10 17:00:33 -0500

    help i have no idea what im doing

--- Week 5 Code/Meckley_Week5_Uni_cellular Assignment.R
+++ Week 5 Code/Meckley_Week5_Uni_cellular Assignment.R
@@ -14,39 +14,7 @@ LM_wd <- function (repo, folder=NULL) {
 
 LM_wd("meckleylg")
 
-# First, recreate Figure 4 from Herron et al. (2019). De novo origins of multicellularity in response to predation. Scientific reports.
-  # Search datadryad.org by the paper title and download the dataset. It will include .csv files and R scripts, organized by figure.
-  # Save the script and change the working directory on lines 8 and 115 to match your computer
-  # Upload the plot you've created to GitHub. (4 points)
-  # Zoom into your plot to look at the distribution for different strains.
-
-
-# Do all of the strains in the plot have the same distributions (yes/no)? (1 pt)
-  #All the strains do.... 
-
-# Based on these observations of your strain distributions, why did the authors use a Kruskal-Wallis test rather than ANOVA to compare the strains? (3 pts)
-  #The authors use a Kruskal-Wallis test rather than ANOVA to compare the strains because... (i forget what you do if a test doenst work)
-
-# Use the fitdist() and gofstat() functions to compare the poisson, negative binomial, and logistic distributions for:
-  # (1) - The number of cells of progeny (data$Num.Cells.Progeny)
-  # (2) - The replication time (data$RepTime.sec)
-    #HINT- "Num.Cells.Progeny" has defined breaks. To display results, use the formula with the "chisqbreaks" argument as follows:
-      #gofstat(list(fit.1, fit.2, fit.3, etc), chisqbreaks=c(1,2,4,8,16,32,64))
-
-
-# Based on the AIC scores, which distribution is the best fit for: (5 pts each)
-  # (1) - The number of cells of progeny (data$Num.Cells.Progeny)?
-  # (2) - The replication time (data$RepTime.sec)?
-
-
-# Plot a generic histogram for the replication time (data$RepTime.sec) (4 pt)
-
-
-# Based on the patterns of this histograms and Figure 4:
-  #Give one hypothesis for an evolutionary process represented by the two tallest bars in your histogram. (8 pts)
-  # Don't cheat by looking at the paper! 
-    # This hypothesis does not need to be correct - it only needs to be ecologically rational based these two figures.
-
+      #########
 
 ### Script to  analyze chlamy timelapse data ###
 install.packages("ggplot2")
@@ -56,18 +24,12 @@ install.packages("reshape2")
 library(reshape2)
 
 LM_wd(repo="meckleylg", folder="Week 5 Code")
-data <- read.csv(file=("/Users/matt/Documents/Dropbox/LifeCycleRevision/ScientificReports/Dryad/Figure4Data.csv"), header=T)
 data <- read.csv(file=("Figure4Data.csv"), header=T)
 
 # Originally (in 2017-04-12 script) I used unweighted means for ordering strains
 #  but used weighted means for plotting size distributions
 # In this current script, I use weighted means for both plotting and calculating mean progeny sizes
 # In other words, I use weighted means for everything in this analysis.
-
-
-
-
-
 
 
 # The following weights cell counts of progeny size data by proportion biomass
@@ -167,20 +129,74 @@ myplot <- ggplot(data = data.weight,
   annotate('text', x=8, y=-0.5, label="115") +
   annotate('text', x=9, y=-0.5, label="103")
 
-ggsave(filename=paste("/Users/matt/Documents/Dropbox/LifeCycleRevision/ScientificReports/Dryad/",
-                      paste("Figure4", ".png", sep=""), sep=""), plot=myplot,
-       width = 10, height = 4, bg = 'transparent')
-
 ggsave(filename=paste("meckleylg", folder="Week 5 Code",
                       paste("Figure4", ".png", sep=""), sep=""), plot=myplot,
        width = 10, height = 4, bg = 'transparent')
 
+library(fitdistrplus)
+library(logspline)
+
+  #########
 
 
+# First, recreate Figure 4 from Herron et al. (2019). De novo origins of multicellularity in response to predation. Scientific reports.
+  # Search datadryad.org by the paper title and download the dataset. It will include .csv files and R scripts, organized by figure.
+  # Save the script and change the working directory on lines 8 and 115 to match your computer
+  # Upload the plot you've created to GitHub. (4 points)
+  # Zoom into your plot to look at the distribution for different strains.
 
 
+            ###I CANT ZOOM IN HELP ME HOW DO I DO THIS???###
 
 
+# Do all of the strains in the plot have the same distributions (yes/no)? (1 pt)
+  #All the strains do not have the same distributions. WRONG HELP ME
 
+# Based on these observations of your strain distributions, why did the authors use a Kruskal-Wallis test rather than ANOVA to compare the strains? (3 pts)
+  #The authors use a Kruskal-Wallis test rather than ANOVA to compare the strains because the data is strains in the plot do not have rge same distributions and there are many outliers, and some stray very far.
+###RIGHT???# will we talk about this more?
+
+# Use the fitdist() and gofstat() functions to compare the poisson, negative binomial, and logistic distributions for:
+  # (1) - The number of cells of progeny (data$Num.Cells.Progeny)
+fitdist(data$Num.Cells.Progeny)
+
+fitdist(list(fit.weibull, fit.gamma, fit.norm, fit.logis), chisqbreaks=c(1,2,4,8,16,32,64))
+
+one.col <- lim_nolog$Ara_plusProg
+
+fit.norm <- fitdist(data$Num.Cells.Progeny, distr = "norm")
+
+gofstat(data$Num.Cells.Progeny)
+gofstat(list(fit.weibull, fit.gamma, fit.norm, fit.logis), chisqbreaks=c(1,2,4,8,16,32,64))
+
+  # (2) - The replication time (data$RepTime.sec)
+fitdist(data$RepTime.sec)
+fit.norm <- fitdist(data$RepTime.sec, distr = "norm")
+
+gofstat(list(fit.weibull, fit.gamma, fit.norm, fit.logis), chisqbreaks=c(1,2,4,8,16,32,64))
+
+gofstat(data$RepTime.sec)
+
+    #HINT- "Num.Cells.Progeny" has defined breaks. To display results, use the formula with the "chisqbreaks" argument as follows:
+      #gofstat(list(fit.1, fit.2, fit.3, etc), chisqbreaks=c(1,2,4,8,16,32,64))
+
+
+# Based on the AIC scores, which distribution is the best fit for: (5 pts each)
+  # (1) - The number of cells of progeny (data$Num.Cells.Progeny)?
+    #
+
+  # (2) - The replication time (data$RepTime.sec)?
+    #
+
+
+# Plot a generic histogram for the replication time (data$RepTime.sec) (4 pt)
+hist(data$RepTime.sec)
+
+
+# Based on the patterns of this histograms and Figure 4:
+  #Give one hypothesis for an evolutionary process represented by the two tallest bars in your histogram. (8 pts)
+  # Don't cheat by looking at the paper! 
+    # This hypothesis does not need to be correct - it only needs to be ecologically rational based these two figures.
+      #
 
 
