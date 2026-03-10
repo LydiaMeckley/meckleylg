9408d8bf065f5675f53d5b53c63c10ede7b7e85d
LydiaMeckley <meckleyl@susqu.edu>
2026-03-10 16:41:56 -0400

    what?

--- Week 8 Code/Meckley_Week8_Communities_assignment.R
+++ Week 8 Code/Meckley_Week8_Communities_assignment.R
@@ -14,7 +14,6 @@ LM_wd <- function (repo, folder=NULL) {
 
 LM_wd("meckleylg")
 
-  #colnames df2 (df1)
 # Load the vegan package and the "SR_Inverts" csv from GitHub.
 # These data are benthic macroinvertebrate samples from the West Branch Susquehanna River and White Deer Hole Creek collected in 2012.
 # Sample codes refer to a riffle number for the West Branch, with smaller numbers upstream (R5, R6, etc.).
@@ -33,25 +32,50 @@ df <- data[,-1:-2]
 
 #Now transpose the data - remember species need to be columns and sites need to be rows for analysis:
 df2 <- t(df) #now make the first row into column names
+colnames(df2) <- df$Sample
+df3 <- df2[-1,]
+
+    #IT WORKS WHEN I DO IT MY WAY BUT NOT HIS WAY???# (ASK FIRST)
 
 # Use these two lines to turn your first row into column names and then remove the first row.
   # This assumes your data frame is named "df". You are welcome to change that.
-names(df) <- lapply(df[1, ], as.character)
-df <- df[-1,] 
+names(df2) <- lapply(df2[1, ], as.character)
+df3 <- df2[-1,] 
 
 # You should now have a data frame with samples as row names and species as column names.
     # The first column should be the riffle where these samples were collected.
 
 #Data analysis (10 points):
 # It is common in community ecology to ask questions about "clustering" of communities spatially (i.e. are closer samples more similar?).
-# Now test if "riffle" is a significant predictor of the macroinvertebrate community.  
+# Now test if "riffle" is a significant predictor of the macroinvertebrate community.
+head(df3)
+
+  #SAYS THAT IT NEEDS TO BE A DATAFRAME FOR THIS TO WORK# (ASK ALONG WITH THAT)
+
+df3.dataframe <- as.data.frame(df3)
+
+  #TELLS ME THERE ARE NAs????# (ASK SECOND - ITS WHEN I CHANGE THE BUGS TO NUMERIC) 
+
+mod1 <- rda(riffle ~ bugs, df3.dataframe)
+mod1
+anova(mod1)
+
   # Report your p-value and constrained variance for the model.
+
+
   # Plot Axis 1 and Axis 2 of the results with 95% confidence intervals around the riffles.
     # Hint: it will make things easier if you create two separate data frames. One with the Riffle names and one with the bugs.
+plot(mod1, type="n", display = c("sites", "scores"))
+text(mod1, display="sites", labels = as.character(df3$riffle))
+pl <- ordiellipse(mod1, df3$riffle, kind="se", conf=0.95, lwd=2, draw = "polygon", 
+                  col="skyblue", border = "blue")
 
 # If your code results in an "error: 'x' must be numeric" Then run this line of code to force all bugs to numeric
   # Assuming your data frame of macroinvertebrates is called "bugs".
-bugs <- sapply(bugs, as.numeric )
+bugs <- sapply(df3, as.numeric)
+riffle <- sapply(df3, as.character)
+
+  #THIS IS NOT WORKING# HMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 
 # (Q1) - Which group of samples is clearly different along Axis 1? Does this make sense based on what you know about the data? (3 pts)
 
@@ -59,9 +83,117 @@ bugs <- sapply(bugs, as.numeric )
 # Use the rarefaction function from the tutorial to plot 250-individual subsamples grouped and summed by the riffle where they were collected.
   # Hint: use the subset() function to select only the samples from a specific riffle
   # Hint 2: use two equal signs, not one, in the subset() function.
+rarefaction<-function(x,subsample=5, plot=TRUE, color=TRUE, error=FALSE, legend=TRUE, symbol=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)){
+  
+  library(vegan)
+  
+  
+  
+  x <- as.matrix(x)
+  y1<-apply(x, 1, sum)
+  rare.data<-x                                   
+  
+  select<-unique(sort(c((apply(x, 1, sum)), (seq(0,(max(y1)), by=subsample)), recursive=TRUE)))
+  
+  
+  storesummary.e<-matrix(data=NA, ncol=length(rare.data[,1]),nrow=length(select))
+  rownames(storesummary.e)<-c(select)
+  colnames(storesummary.e)<-rownames(x)
+  storesummary.se<-matrix(data=NA, ncol=length(rare.data[,1]),nrow=length(select))
+  rownames(storesummary.se)<-c(select)
+  colnames(storesummary.se)<-rownames(x)
+  
+  
+  
+  
+  for(i in 1:length(select))                      #the for loop
+  {
+    select.c<-select[i]                     #assigns the 'i'th element of select to select.c
+    foo<-rarefy(x,select.c, se=T)           #use whatever vegan fn you want
+    
+    
+    
+    storesummary.e[i,]<-foo[1,]
+    storesummary.se[i,]<-foo[2,]            
+    
+  }
+  
+  storesummary.e<-as.data.frame(storesummary.e)               
+  richness.error<<-storesummary.se
+  
+  for (i in 1:(length(storesummary.e)))
+  {
+    storesummary.e[,i]<-ifelse(select>sum(x[i,]), NA, storesummary.e[,i])
+  }
+  
+  
+  
+  ###############plot result################################
+  if (plot==TRUE)
+  {
+    if(color==TRUE){
+      plot(select,storesummary.e[,1], xlab="Individuals in Subsample", 
+           xlim=c(0,max(select)), ylim=c(0, 5+(max(storesummary.e[,1:(length(storesummary.e))], na.rm=TRUE))),
+           ylab="Mean Species Richness", pch =16, col=2, type="n")
+      
+      for (j in 1:(length(storesummary.e))){
+        points(select, storesummary.e[,j], pch=16, col=j+1, type="b", lty=1)}
+      
+      if(error==TRUE){
+        for (m in 1:(length(storesummary.e))){
+          segments(select, storesummary.e[,m]+storesummary.se[,m],select, storesummary.e[,m]-storesummary.se[,m])
+        }
+      }
+      if (legend==TRUE){
+        legend("bottomright", colnames(storesummary.e), inset=0.05, lty=1, col=1:length(storesummary.e)+1, lwd=2)
+      }
+    }
+    else
+    {
+      plot(select,storesummary.e[,1], xlab="Individuals in Subsample", 
+           xlim=c(0,max(select)), ylim=c(0, 5+(max(storesummary.e[,1:(length(storesummary.e))], na.rm=TRUE))),
+           ylab="Mean Species Richness", pch =16, col=2, type="n")
+      
+      for (j in 1:(length(storesummary.e))){
+        points(select, storesummary.e[,j], type="l", lty=1)}
+      
+      for (k in 1:(length(storesummary.e))){
+        symbol<-ifelse(symbol<length(storesummary.e),rep(symbol,2),symbol)
+        points(as.numeric(rownames(subset(storesummary.e, storesummary.e[,k]==max(storesummary.e[,k],na.rm=TRUE)))), max(storesummary.e[,k],na.rm=TRUE), pch=symbol[k], cex=1.5)}
+      
+      if(error==TRUE){
+        for (m in 1:(length(storesummary.e))){
+          points(select, storesummary.e[,m]+storesummary.se[,m], type="l", lty=2)
+          points(select, storesummary.e[,m]-storesummary.se[,m], type="l", lty=2)}}
+      
+      k<-1:(length(storesummary.e))
+      if (legend==TRUE){
+        legend("bottomright", colnames(storesummary.e), pch=symbol[k], inset=0.05, cex=1.3)
+      }
+    }
+  }
+  print("rarefaction by J. Jacobs, last update April 17, 2009")
+  if(error==TRUE)(print("errors around lines are the se of the iterations, not true se of the means")  )     
+  list("richness"= storesummary.e, "SE"=richness.error, "subsample"=select)        
+  
+}
+
+samples <- as.data.frame(t(rowSums(t(bugs))))
+
+rarefaction(samples, subsample=250, plot=TRUE, color=TRUE, error=FALSE,  legend=TRUE, symbol)
+
+  
+
+#I ACTUALLT HAVE NO IDEA WHAT IS GOING ON HERE...# - getting weird errors.
+
+
 # (Q2) - Which riffle took the most effort to effectively sample? (2 pts)
     # Hint: if you use rbind() to bring your summed riffles together it will be easier to display in a single rarefaction plot.
+
+
 # (Q3) - Do you think the differences between riffles are ecologically meaningful? (3 pts)
     # Hint: It might help to look at 800-individual subsamples to answer this question.
+
+
 # (Q4) - Why do the curves stop at different locations on the x-Axis? (2 pts)
 
