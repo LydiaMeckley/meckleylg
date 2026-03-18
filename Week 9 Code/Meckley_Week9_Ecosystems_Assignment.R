b34855c87214787e5510ac4434689309065d4d6f
LydiaMeckley <meckleyl@susqu.edu>
2026-03-18 19:23:10 -0400

    nothing is significant ._.

--- Week 9 Code/Meckley_Week9_Ecosystems_Assignment.R
+++ Week 9 Code/Meckley_Week9_Ecosystems_Assignment.R
@@ -25,7 +25,6 @@ LM_wd(repo="R4Eco_2026", folder="Week9")
 
 # (Q1 - 12 pts) Use the dataset from the tutorial to complete one redundancy analysis (RDA) with variance partitioning on a different community (NOT the nematodes).
   # Explain the ecological importance of your significant predictor variables, or the importance if none are significant for your community.
-    ###FIND IN WEEK 8### - same as first half of last assignment
 invert.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Invertebrate_community")
 invert <- as.data.frame(invert.tibble)
 head(invert)
@@ -37,7 +36,7 @@ invert.df1 <- as.data.frame(sapply(invert.df[3:71], as.numeric))
 abiotic.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Abiotic factors")
 abiotic.df <- as.data.frame(abiotic.tibble)
 
-      ###IS PARCEL SOMETHING I SHOULD BE COMBINING###
+      ###IS PARCEL SOMETHING I SHOULD BE COMBINING???###
 
 abiotic.df$names <- paste(abiotic.df$Parcel, abiotic.df$Land_Use)
 invert.df1$names <- paste(invert.df$Parcel, invert.df$Landuse)
@@ -46,13 +45,13 @@ invert.means <- aggregate(x = invert.df1, by = list(invert.df1$names), FUN = mea
 
 invert.df$Phenacolimax_major
 abiotic.mean1 <- abiotic.means[,-16]
-abiotic.mean2 <- abiotic.mean1[,-1:-6] # Plot and NA columns
-abiotic.mean2 <- sapply(abiotic.mean2, as.numeric ) # Make sure everything is numeric.
-abiotic.mean2 <- as.data.frame(abiotic.mean2) # Make sure it's in the right format.
+abiotic.mean2 <- abiotic.mean1[,-1:-6]
+abiotic.mean2 <- sapply(abiotic.mean2, as.numeric )
+abiotic.mean2 <- as.data.frame(abiotic.mean2)
 invert.mean1 <- invert.means[,-1]
 
 invert.mean1 <- invert.mean1[-5,]
-condition
+#condition
 
 col_sums <- colSums(invert.mean1[,-70])
 condition <- col_sums > 0
@@ -63,18 +62,52 @@ print(invert.mean2)
 
 ord <- rda(invert.mean2 ~ pH + totalN + Perc_ash + Kalium + Magnesium + Ca + Al + TotalP + OlsenP, abiotic.mean2)
 ord
+anova(ord)
+  
+      ###IS MY NUMBER OKAY HERE???  0.4623 ### bad p value like the tutorial
+
 ord <- rda(invert.mean2 ~., abiotic.mean2)
+ord.int <- rda(invert.mean2 ~1, abiotic.mean2) # shorthand for the model that only includes intercepts.
 step.mod <- ordistep(ord.int, scope = formula(ord), selection = "both")
-step.mod$anova
-step.R2mod <- ordiR2step(ord.int, scope = formula(ord), selection = "forward")
-ord2 <- rda(nema.means2 ~ totalN, abiotic.means2)   ###ADD WHATEVER I THINK PREDICTS WHAT###
+anova(step.mod)
+
+      ###NONE ARE SIGNIFICANT??? HOW DO I CONTINUE THEN???###
+
+step.R2mod <- ordiR2step(ord.int, scope = formula(ord), selection = "forward") 
+
+ord2 <- rda(nema.means2 ~ totalN, abiotic.means2)   ###DO I EVEN NEED THIS???###
 ord2
 anova(ord2)
 plot(ord2)
 
 # (Q2 - 12 pts) Then use the dataset from the tutorial to create a linear model related to your RDA. Try multiple predictors to find the best fit model.
   # Explain the ecological importance of the significant predictors, or lack of significant predictors.
-    ###when I get done ask him to make sure I did it right
+
+    ###DO I BRING IN ANOTHER DATASET OR JUST GO BASED ON THE ONES I HAVE ABOVE???###
+
+fit.weibull <- fitdist(soil.plants$Leaves, distr = "weibull")
+fit.norm <- fitdist(soil.plants$Leaves, distr = "norm")
+fit.gamma <- fitdist(soil.plants$Leaves, distr = "gamma")
+fit.lnorm <- fitdist(soil.plants$Leaves, distr = "lnorm")               ###WHAT WOULD I ADD INTO THESE???###
+fit.nbinom <- fitdist(soil.plants$Leaves, distr = "nbinom")       ###IM SCARED :(###
+fit.logis <- fitdist(soil.plants$Leaves, distr = "logis")
+fit.geom <- fitdist(soil.plants$Leaves, distr = "geom")
+gofstat(list(fit.weibull, fit.norm, fit.gamma, 
+             fit.lnorm, fit.nbinom, fit.logis, fit.geom))
+
+mod1 <- lm(Leaves ~ pH + totalN + Kalium + Magnesium + Ca + Al + TotalP + Land_use + Species_code, data = soil.plants)
+summary(mod1)   
+anova(mod1)
+AIC(mod1)
+summary(mod1)$adj.r.squared
+
+mod2 <- lm(Leaves ~ pH + totalN + Kalium + Species_code,soil.plants)
+summary(mod2)
+anova(mod2)
+AIC(mod1,mod2)
+
+plot(mod2$residuals)
+summary(mod2)$adj.r.squared
 
 
 #what was the thread between nematodes and nitrogen? PLANTS!!!
