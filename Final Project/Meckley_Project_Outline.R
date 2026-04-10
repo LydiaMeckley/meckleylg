68173369f5704222d80244cdfdabb8cf23c3e81c
LydiaMeckley <meckleyl@susqu.edu>
2026-04-10 18:29:23 -0400

    outlinezzzzzzzzzzzz

--- Final Project/Meckley_Project_Outline.R
+++ Final Project/Meckley_Project_Outline.R
@@ -16,51 +16,30 @@ LM_wd("meckleylg")
 
 # 1.
   #What coastal seawater factors of England are capable of predicting both the harbor seal and grey seal populations over the course of 
-  #three decades (1990-2020)?
+  #about three decades (1990-2018)?
 
 # 2.
-  #
-
-###In the seal one, maybe start at 2009 because both sits 'start' at 2009 
-  ###But also maybe only use the chich place because it has all the dates
-      #all places in South East England
-        #is there a way to sort out the rest of the places and only keep South East
-
-#What changing factors in the ocean affect the two seal populations differently.
-
-###DUE THURSDAY###
-
-###add a region column
-
-#water$side_region <- water$side
-#water$side_region <- gsub(water, "South East", "SE subregion")
-#and then region 2
-
-
-
-###DONT COMPARE YEAR, 
-
-###CH - 1999-
-
-#use i naturalist to get datasets into R
-
-#RGBIF
-
+  #My first dataset, which is the coastal seawater factors, comes from Cefas, more specifically, their data portal. The CSV is given below:
+LM_wd(repo="meckleylg", folder="Final Project")
+data <- read.csv("UK_Water_Data.csv")
+  #The title of this dataset is called: "Dissolved oxygen, temperature and salinity measurements in coastal and estuarine waters in the UK from 1990 to 2018".
+  
+  #My second dataset, which is the count data of both the harbor and grey seals over time in the UK, comes from the iNaturalist database.
+  #I got this data with the help of Google Gemini
+  #The code to retrieve this data is below:
 install.packages("rgbif")
 library(rgbif)
 library(dplyr)
 
-      ################THIS WORKS NOW##################
-
-# 1. Species Keys
+      # 1. Species Keys
 h_key <- name_backbone("Phoca vitulina")$usageKey
 g_key <- name_backbone("Halichoerus grypus")$usageKey
 inat_dataset <- "50c9509d-22c7-4a22-a47d-8c48425ef4a7"
 
-# 2. Define the years we want
-years <- 1990:2020
+      # 2. Define the years we want
+years <- 1990:2018
 
-# 3. Create a helper function to fetch one year at a time
+      # 3. Create a helper function to fetch one year at a time
 get_yearly_data <- function(yr, species_key, label) {
   # We add a small pause (0.5s) to avoid hitting rate limits
   Sys.sleep(0.5)
@@ -76,12 +55,37 @@ get_yearly_data <- function(yr, species_key, label) {
   return(data.frame(year = yr, count = cnt, species = label))
 }
 
-# 4. Run the loop (this might take a minute, but it's safer)
+      # 4. Run the loop (this might take a minute, but it's safer)
 harbor_list <- lapply(years, get_yearly_data, species_key = h_key, label = "Harbor Seal")
 grey_list   <- lapply(years, get_yearly_data, species_key = g_key, label = "Grey Seal")
 
-# 5. Combine results
+      # 5. Combine results
 seal_data <- bind_rows(harbor_list, grey_list)
+
+# 3.
+  #From the coastal water data, I plan on using data ranging from the dates specified for this project, which is what they are in the dataset.
+  #The columns I plan on using are "Country", "Year", "Oxygen", "Temp", and "Salinity". These will be used to predict the seal populations.
+  #From the seal dataset, I plan on using the "year", "count", and "species" columns. These are what is being compared with the coastal water data.
+
+# 4.
+  #The statistics that I plan on using to analyze this dataset are...
+
+# 5. 
+  #Two figures that I think might be most useful based on the datasets I am comparing are...
+
+# 6. 
+  #The data processing methods that I will be using to bring the data into a useable format for my statistics and figures is that...
+
+
+
+
+
+###DUE THURSDAY###
+
+#add a region column
+#water$side_region <- water$side
+#water$side_region <- gsub(water, "South East", "SE subregion")
+#and then region 2
 
 ############## JUST INCASE I NEED THIS AS A BACKUP ##################
 
@@ -96,7 +100,7 @@ harbor_counts <- galah_call() %>%
   galah_filter(
     scientificName == "Phoca vitulina",
     year >= 1990, 
-    year <= 2020
+    year <= 2018
   ) %>%
   galah_group_by(year) %>%
   atlas_counts() %>%
@@ -107,7 +111,7 @@ grey_counts <- galah_call() %>%
   galah_filter(
     scientificName == "Halichoerus grypus",
     year >= 1990, 
-    year <= 2020
+    year <= 2018
   ) %>%
   galah_group_by(year) %>%
   atlas_counts() %>%
