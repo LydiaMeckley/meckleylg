7877c5d8d48583cc37267c92cc2c731e75a2e8db
LydiaMeckley <meckleyl@susqu.edu>
2026-04-16 14:09:47 -0400

    project work has started

--- Final Project/LydiaM_Final_Project.R
+++ Final Project/LydiaM_Final_Project.R
@@ -28,23 +28,59 @@ library(fitdistrplus)
 library(logspline)
 
 ###DATASETS###
+  #COASTAL WATER DATA#
 LM_wd(repo="meckleylg", folder="Final Project")
-data <- read.csv("UK_Water_Data.csv")
+waterdata <- read.csv("UK_Water_Data.csv")
 
+  #SEAL DATA#
+# 1. Species Keys
+h_key <- name_backbone("Phoca vitulina")$usageKey
+g_key <- name_backbone("Halichoerus grypus")$usageKey
+inat_dataset <- "50c9509d-22c7-4a22-a47d-8c48425ef4a7"
 
-###HOW TO FIX THE SEAL DATA ACCORDING TO AI BUT ASK HOW TO ACTUALLY DO IT###
-library(tidyr)
+# 2. Define the years we want
+years <- 1990:2018
 
-df_wide <- df %>%
-  pivot_wider(
-    names_from = species, 
-    values_from = count
+# 3. Create a helper function to fetch one year at a time
+get_yearly_data <- function(yr, species_key, label) {
+  # We add a small pause (0.5s) to avoid hitting rate limits
+  Sys.sleep(0.5)
+  
+  # Fetch count
+  cnt <- occ_count(
+    taxonKey = species_key,
+    country = "GB",
+    datasetKey = inat_dataset,
+    year = yr
   )
-df_wide <- df %>%
-  pivot_wider(
-    names_from = species,
-    values_from = count,
-    values_fill = 0
-  )
+  
+  return(data.frame(year = yr, count = cnt, species = label))
+}
+
+# 4. Run the loop (this might take a minute, but it's safer)
+harbor_list <- lapply(years, get_yearly_data, species_key = h_key, label = "Harbor Seal")
+grey_list   <- lapply(years, get_yearly_data, species_key = g_key, label = "Grey Seal")
+
+# 5. Combine results
+sealdata <- bind_rows(harbor_list, grey_list)
+
+
+                    #####CLEANING UP THE DATA#####
+
+
+###REMOVE COLUMNS I DO NOT NEED FROM THE COASTAL WATER DATASET###
+waterdata <- waterdata[,-1]
+waterdata <- waterdata[,-2:-6]
+waterdata <- waterdata[,-3:-7]
+waterdata <- waterdata[,-6]
+
+###SUBSET THE COASTAL WATER DATA TO ONLY HAVE DATA FROM THE COUNTRY "ENGLAND"###
+waterdata <- subset(waterdata, Country == "England")
+
+###MEAN THE COASTAL WATER DATA BY YEAR###
+water.means <- aggregate(x = waterdata, by = list(abiotic$names), FUN = "mean")
+
+###MERGE BOTH OF THE DATASETS (COSTAL WATER DATA AND SEAL POPULATION DATA)###
+merge(x, y, by.x = "Year", by.y = "year")
 
 
